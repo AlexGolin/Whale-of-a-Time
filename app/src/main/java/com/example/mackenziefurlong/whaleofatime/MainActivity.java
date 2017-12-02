3b93ca6a4ad7faddcbce102aecad4fe475a054c0
mack6870 <mackenziefurlong@hotmail.com>
2017-12-02 13:49:37 -0500

    Here's the base

--- app/src/main/java/com/example/mackenziefurlong/whaleofatime/MainActivity.java
+++ app/src/main/java/com/example/mackenziefurlong/whaleofatime/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.mackenziefurlong.whaleofatime;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.*;
+import android.app.ActionBar;
 
 /**
  * Created by mackenziefurlong on 2017-12-02.
@@ -14,5 +16,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }

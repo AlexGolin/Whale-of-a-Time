7b8b0b49fc5b15244966685c191e276efeaa5fde
mack6870 <mackenziefurlong@hotmail.com>
2017-12-02 11:39:03 -0500

    added a main activity

---
+++ app/src/main/java/com/example/mackenziefurlong/whaleofatime/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.mackenziefurlong.whaleofatime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by mackenziefurlong on 2017-12-02.
+ */
+
+
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
